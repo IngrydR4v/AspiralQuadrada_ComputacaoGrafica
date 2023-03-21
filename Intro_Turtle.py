17d408d9426e0ca7fcb4492bbc54b4aad1640155
Ingryd Thaiany Ravanelli Galon <67930165+IngrydR4v@users.noreply.github.com>
2023-03-20 21:19:49 -0300

    Add files via upload

---
+++ Intro_Turtle.py
@@ -0,0 +1,21 @@
+#import turtle
+#
+# turtle.pensize(5)
+#
+# turtle.forward(300)
+# turtle.left(90)
+# turtle.forward(300)
+# turtle.left(90)
+# turtle.forward(300)
+# turtle.left(90)
+# turtle.forward(300)
+# turtle.left(90)
+#
+# turtle.Screen().exitonclick()
+import turtle
+turtle.pensize(5)
+for i in range(6):
+    turtle.forward(100)
+    turtle.left(60)
+
+turtle.Screen().exitonclick()
